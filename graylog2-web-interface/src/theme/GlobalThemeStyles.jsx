bae0bfeb74accf85871bf5c1cd67875c0ddf0424
Kyle Knight <kyle@graylog.com>
2019-12-10 14:40:37 -0600

    Updated Pagination and Deprecated Pagination

--- graylog2-web-interface/src/theme/GlobalThemeStyles.jsx
+++ graylog2-web-interface/src/theme/GlobalThemeStyles.jsx
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { paginationStyles } from 'components/graylog/Pagination.jsx';
 import { progressBarStyles } from 'components/graylog/ProgressBar.jsx';
 
 const GlobalThemeStyles = createGlobalStyle`
+  ${paginationStyles};
   ${progressBarStyles};
 `;
 
